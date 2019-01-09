0afc5dd128293f3f381b7eb39718f02e6d0786eb
Xandra <xandrabest@gmail.com>
2019-01-09 15:28:32 -0600

    fix incorrect rename filepath and add date based file naming

--- Imports/Utilities/fileManagement.py
+++ Imports/Utilities/fileManagement.py
@@ -1,14 +1,20 @@
 from .const import *
+from .dateTime import *
 
 import shutil
 import os
+
+def generateAssemblyOrderFileName():
+	return ASSEMBLY_ORDER_FILE_NAME_PREFIX + getDateTimeAsFilePathSegment() + EXCEL_FILE_NAME_SUFFIX
+
+def generateAssemblyOrderFilePath():
+	return os.path.join(ASSEMBLY_ORDERS_DIR_PATH, generateAssemblyOrderFileName())
 
 def createAssemblyOrderFile():
 	shutil.copy2(TEMPLATE_FILE_NAME_PATH, ASSEMBLY_ORDERS_DIR_PATH)
 
 def renameAssemblyOrderFile():
-	destinationFilePath = os.path.join(ASSEMBLY_ORDERS_DIR_PATH, "sillyName.xlsx")
-	shutil.move(TEMPLATE_FILE_NAME_PATH, destinationFilePath)
+	shutil.move(ASSEMBLY_ORDER_FILE_NAME_PATH, generateAssemblyOrderFilePath())
 
 def deleteAssemblyOrderFile():
 	os.remove(ASSEMBLY_ORDER_FILE_NAME_PATH)
