b5669c339cf4f79425593992397ca0d54d8261c7
Xandra <xandrabest@gmail.com>
2019-01-11 04:23:10 -0600

    populate assembly file with relevant data

--- Imports/Utilities/excel.py
+++ Imports/Utilities/excel.py
@@ -1,7 +1,9 @@
 from .const import *
 from .fileManagement import *
+from .sys import *
 from ..Classes.Component import *
 from ..Classes.Task import *
+
 
 from openpyxl import load_workbook
 
@@ -24,7 +26,7 @@ def generateTaskList():
 	taskList = []
 
 	workBook = loadWorkbook(TASKS, selectTaskFile(), True)
-	taskSheet = workBook['tasks']
+	taskSheet = workBook[TASK_FILE_TASK_SHEET]
 
 	productDictionary = generateProductDictionary(workBook)
 
@@ -40,7 +42,7 @@ def generateTaskList():
 
 
 def generateProductDictionary(workBook):
-	componentSheet = workBook['components']
+	componentSheet = workBook[TASK_FILE_COMPONENTS_SHEET]
 	# TODO assert worksheet dimensions from ws.calculate_dimension() sheet.get_highest_column()
 	# TODO assert column headers are as expected
 	# TODO use regex to assert expected formatting of rows
@@ -58,7 +60,46 @@ def generateProductDictionary(workBook):
 	return productDictionary
 
 
+def generateAssemblyOrderFile(taskList):
+	createAssemblyOrderFile()
+	try:
+		populateAssemblyOrderFile(taskList)
+		renameAssemblyOrderFile()
+	except:
+		print('Something went wrong! Cancelling process.')
+		deleteAssemblyOrderFile()
+		quit()
+
+
 def populateAssemblyOrderFile(taskList):
 	workBook = loadWorkbook(TEMPLATE, ASSEMBLY_ORDER_FILE_NAME_PATH)
-	for i in range (0, len(taskList)):
-		pass
+	templateSheet = workBook[TEMPLATE_FILE_TEMPLATE_SHEET]
+	taskCount = len(taskList)
+
+	for i in range (0, taskCount):
+		product = taskList[i].getProduct()
+		newSheet = workBook.copy_worksheet(templateSheet)
+		newSheet.title = generateSheetName(i, product)
+		#TODO nerd out
+		newSheet.cell(row = 1, column = 7).value = generateFraction(i, taskCount);
+		newSheet.cell(row = 2, column = 4).value = product
+		newSheet.cell(row = 4, column = 4).value = taskList[i].getQuantity()
+
+		#TODO implement max
+		currentRow = 8
+		for component in taskList[i].getComponents():
+			newSheet.cell(row = currentRow, column = 1).value = component.getPart()
+			newSheet.cell(row = currentRow, column = 4).value = component.getQuantity()
+			currentRow = currentRow + 1
+
+
+	workBook.remove(templateSheet)
+	workBook.save(ASSEMBLY_ORDER_FILE_NAME_PATH)
+
+
+def generateSheetName(counter, product):
+	return str(counter + 1) + " " + product[:27]
+
+
+def generateFraction(counter, total):
+	return str(counter + 1) + "/" + str(total)
--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from Imports.Utilities.excel import *
-from Imports.Utilities.fileManagement import *
-from Imports.Utilities.sys import *
-from Imports.Utilities.userInteraction import *
 
 def main():
 	clearTerminal()
 	taskList = generateTaskList()
-	createAssemblyOrderFile()
-	try:
-		populateAssemblyOrderFile(taskList)
-		renameAssemblyOrderFile()
-	except:
-		print('Something went wrong! Cancelling process.')
-		deleteAssemblyOrderFile()
-		quit()
-
+	generateAssemblyOrderFile(taskList)
 
 main()
