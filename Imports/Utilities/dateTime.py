6b46d15a8e102849323e86388555c910d590b0c9
Xandra <xandrabest@gmail.com>
2019-01-17 12:20:30 -0600

    refactor constants and add latest task list

--- Imports/Utilities/dateTime.py
+++ Imports/Utilities/dateTime.py
@@ -4,4 +4,4 @@ def getDateTime():
 	return datetime.datetime.now()
 
 def getDateTimeAsFilePathSegment():
-	return getDateTime().strftime("%Y_%m_%d_%H%M")
+	return getDateTime().strftime('%Y_%m_%d_%H%M')
